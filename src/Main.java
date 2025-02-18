0576b2d6bde44929afab22cf67aa4e71da300088
scotiana2415 <scotiana2004@gmail.com>
2025-02-18 12:03:46 +0200

    ex 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import Model.Charakter;
+import Model.Produkt;
+import Repository.Repository;
+import Service.Service;
+import ConsoleView.ConsoleView;
+import Controller.Controller;
+import Repository.Repository;
 public class Main {
     public static void main(String[] args) {
+        // Create repositories with explicit types
+        Repository<Charakter> charakterRepo = new Repository<Charakter>();
+        Repository<Produkt> produktRepo = new Repository<Produkt>();
 
+        // Create services
+        Service<Charakter> charakterService = new Service<Charakter>(charakterRepo);
+        Service<Produkt> produktService = new Service<Produkt>(produktRepo);
 
+        // Create controller
+        Controller controller = new Controller(charakterService, produktService);
+
+        // Create and start console view
+        ConsoleView view = new ConsoleView(controller);
+        view.run();
     }
 }
+
 
